798e88355fcd6c16da8c0726861c1ea0f8ffa153
garykhong <gary_khong@yahoo.com>
2017-07-15 17:03:45 +1000

    add Bubble sort assignment

--- TechnicalInterviewAssignment.Tests/BubbleSorter_SortNumbers.cs
+++ TechnicalInterviewAssignment.Tests/BubbleSorter_SortNumbers.cs
@@ -13,6 +13,14 @@ namespace TechnicalInterviewAssignment.Tests
             BubbleSorter bubbleSorter = new BubbleSorter(new int[] { 1, 2, 3 });
             CollectionAssert.AreEqual(new int[] { 1, 2, 3 }, bubbleSorter.SortNumbers());
             Assert.AreEqual(0, bubbleSorter.NumberOfSwaps);
-        }       
+        }
+
+        [TestMethod]
+        public void Input321NumbersArray_Gives3SwapsAnd123NumbersArray()
+        {
+            BubbleSorter bubbleSorter = new BubbleSorter(new int[] { 3, 2, 1 });
+            CollectionAssert.AreEqual(new int[] { 1, 2, 3 }, bubbleSorter.SortNumbers());
+            Assert.AreEqual(3, bubbleSorter.NumberOfSwaps);
+        }
     }
 }
--- TechnicalInterviewAssignment/BubbleSorter.cs
+++ TechnicalInterviewAssignment/BubbleSorter.cs
@@ -3,7 +3,7 @@
     public class BubbleSorter
     {
         private int numberOfSwaps;
-        public int NumberOfSwaps { get { return NumberOfSwaps; } }
+        public int NumberOfSwaps { get { return numberOfSwaps; } }
         private int[] Numbers { get; set; }
 
         public BubbleSorter(int[] numbers)
@@ -13,7 +13,30 @@
 
         public int[] SortNumbers()
         {
-            return new int[] { 1, 2, 3 };
+            bool continueSorting = true;
+            while(continueSorting)
+            {
+                continueSorting = false;
+                for(int i = 0; i < Numbers.Length - 1; i++)
+                {
+                    if(Numbers[i] > Numbers[i + 1])
+                    {
+                        SwapNumberWithNextNumberInNumbers(i);
+                        numberOfSwaps++;
+                        continueSorting = true;
+                    }
+                }
+            }
+
+            return Numbers;
+        }
+
+        private void SwapNumberWithNextNumberInNumbers(int index)
+        {
+            int number = Numbers[index];
+            int nextNumber = Numbers[index + 1];
+            Numbers[index] = nextNumber;
+            Numbers[index + 1] = number;
         }
 
         
