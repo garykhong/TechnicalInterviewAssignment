0b41edf970c0cbd0c49f6b91be04a9c3b7b96dd8
garykhong <gary_khong@yahoo.com>
2017-08-13 17:05:17 +1000

    use stack for expected brackets

--- TechnicalInterviewAssignment/BracketsBalanceDetector.cs
+++ TechnicalInterviewAssignment/BracketsBalanceDetector.cs
@@ -5,83 +5,53 @@ namespace TechnicalInterviewAssignment
 {
     public class BracketsBalanceDetector
     {
-        private string brackets;
-        private List<BracketsSplitter> splitters;
-        private List<BracketPairsCreator> pairsCreators;
+        private string brackets;        
+        private Stack<BracketPair> leftBrackets;
 
         public BracketsBalanceDetector(string brackets)
         {
-            this.brackets = brackets;
-            splitters = new List<BracketsSplitter>();
-            pairsCreators = new List<BracketPairsCreator>();
+            this.brackets = brackets;            
+            leftBrackets = new Stack<BracketPair>();
         }
 
         public bool AreBracketsBalanced()
-        {
-            SplitBrackets();
-            SetPairsCreatorsFromSplitters();
-            return AreBracketsEvenAmount() && AreBracketPairsAllMatching() && 
-                     AreThereTheExpectedAmountOfBracketPairs();
-        }
-
-        private void SplitBrackets()
-        {
-            splitters.Add(new BracketsSplitter(brackets, "[", "]"));
-            splitters.Add(new BracketsSplitter(brackets, "(", ")"));
-            splitters.Add(new BracketsSplitter(brackets, "{", "}"));
-
-            foreach (BracketsSplitter splitter in splitters)
-                splitter.SplitBrackets();
-        }
-
-        private void SetPairsCreatorsFromSplitters()
-        {
-            pairsCreators = new List<BracketPairsCreator>();
-            foreach (BracketsSplitter splitter in splitters)
+        {            
+            foreach(char bracket in brackets)
             {
-                pairsCreators.Add(new BracketPairsCreator(splitter.LeftBrackets,
-                                         splitter.RightBrackets));
-            }
-        }
-
-        private List<BracketPair> GetBracketPairs()
-        {
-            List<BracketPair> bracketpairs = new List<BracketPair>();
-            foreach(BracketPairsCreator creator in pairsCreators)
-            {
-                foreach(BracketPair pair in creator.GetBracketPairs())
+                switch(bracket)
                 {
-                    bracketpairs.Add(pair);
+                    case '[':
+                        leftBrackets.Push(new BracketPair(new Bracket("[", 0), new Bracket("]", 0)));
+                        break;
+                    case '(':
+                        leftBrackets.Push(new BracketPair(new Bracket("(", 0), new Bracket(")", 0)));
+                        break;
+                    case '{':
+                        leftBrackets.Push(new BracketPair(new Bracket("{", 0), new Bracket("}", 0)));
+                        break;
+                    default:
+                        if(leftBrackets.Count == 0)
+                        {
+                            return false;
+                        }
+                        else
+                        {
+                            var leftBracket = leftBrackets.Pop();
+                            if(leftBracket.SecondBracket.Value != bracket.ToString())
+                            {
+                                return false;
+                            }
+                        }
+
+                        break;
                 }
             }
-            
-            return bracketpairs;
-        }
+            return AreBracketsEvenAmount();
+        }        
 
         private bool AreBracketsEvenAmount()
         {
             return brackets.Length % 2 == 0;
-        }
-
-        private bool AreThereTheExpectedAmountOfBracketPairs()
-        {
-            return GetBracketPairs().Count == brackets.Length / 2;
-        }
-
-        private bool AreBracketPairsAllMatching()
-        {
-            foreach (BracketPair bracketPair in GetBracketPairs())
-            {
-                BracketPairMatchDetector detector =
-                    new BracketPairMatchDetector(bracketPair.FirstBracket, bracketPair.SecondBracket);
-                if (!detector.AreBracketsAMatchingPair())
-                {
-                    return false;
-                }
-            }
-
-            return true;
-        }
-        
+        }       
     }
 }
