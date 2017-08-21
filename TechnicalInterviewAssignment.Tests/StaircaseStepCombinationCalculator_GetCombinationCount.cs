7738e0e8ce93172e4be2e43bb36e9efcef026ac7
garykhong <gary_khong@yahoo.com>
2017-08-21 13:55:15 +1000

    staircase step calculator brute force

---
+++ TechnicalInterviewAssignment.Tests/StaircaseStepCombinationCalculator_GetCombinationCount.cs
@@ -0,0 +1,38 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TechnicalInterviewAssignment;
+
+namespace TechnicalInterviewAssignment.Tests
+{
+    [TestClass]
+    public class StaircaseStepCombinationCalculator_GetCombinationCount
+    {
+        [TestMethod]
+        public void SampleTestCase_GivesExpectedResults()
+        {            
+            Assert.AreEqual(1, 
+                GetCombinationCount(1));
+            Assert.AreEqual(2,
+                GetCombinationCount(2));
+            Assert.AreEqual(4,
+                GetCombinationCount(3));
+            Assert.AreEqual(7,
+                GetCombinationCount(4));
+            Assert.AreEqual(13,
+                GetCombinationCount(5));
+            Assert.AreEqual(7,
+                GetCombinationCount(6));
+            Assert.AreEqual(44,
+                GetCombinationCount(7));
+            Assert.AreEqual(1132436852,
+                GetCombinationCount(35));
+        }
+
+        private int GetCombinationCount(int steps)
+        {
+            StaircaseStepCombinationCalculator calculator =
+                new StaircaseStepCombinationCalculator(steps);
+            return calculator.GetCombinationCount();
+        }
+    }
+}
---
+++ TechnicalInterviewAssignment/StaircaseStepCombinationCalculator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TechnicalInterviewAssignment
+{
+    public class StaircaseStepCombinationCalculator
+    {
+        private int steps;
+        private int totalCombinations;
+
+        public StaircaseStepCombinationCalculator(int steps)
+        {
+            this.steps = steps;
+        }
+
+        public int GetCombinationCount()
+        {
+            CalculateStepCombinationCount(0);
+            return totalCombinations;
+        }
+
+        private void CalculateStepCombinationCount(int stepsTaken)
+        {
+            for(int numberOfStepsToTake = 1; numberOfStepsToTake <= 3; numberOfStepsToTake++)
+            {
+                if(stepsTaken == steps)
+                {
+                    totalCombinations += 1;
+                    return;
+                }
+                else if(stepsTaken > steps)
+                {
+                    return;
+                }
+                else if(stepsTaken < steps)
+                {
+                    CalculateStepCombinationCount(stepsTaken + numberOfStepsToTake);
+                }
+            }
+        }
+    }
+}
